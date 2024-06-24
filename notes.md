92a1838dfa87d8ff9bfd468fe48b56e052848a67
alfarseniko <alfarseniko@gmail.com>
2024-06-24 19:47:17 +0330

    initial commit; contract created

---
+++ notes.md
@@ -0,0 +1,8 @@
+An automated smart contract for ride hailing apps like uber using the zksync sepolia testnet
+
+- given starting location and ending location
+- given the distance 
+- given the waiting time by either driver or passenger
+- price converters from chainlink oracle
+- admin permissions
+- automated payments
---
+++ src/uber.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+
+// Pragma
+pragma solidity ^0.8.24;
+
+// Imports
+
+// Interfaces, Libraries and Contracts
+
+/**
+ @title An automated uber payment app
+ @author Haris Waheed Bhatti
+ @notice This contract is for the payment of fares after rides
+ */
+
+contract Uber {
+    // Type Declarations
+    // State Variables
+    // Modifiers
+    // Functions Order:
+    //// constructor
+    //// receive
+    //// fallback
+    //// external
+    //// public
+    //// internal
+    //// private
+    //// view / pure
+}
