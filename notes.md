4b02efae65ab4aeaa40cb1dfd0657b5c540227e9
alfarseniko <alfarseniko@gmail.com>
2024-06-24 23:30:56 +0330

    update: data structures(structs)

--- notes.md
+++ notes.md
@@ -1,8 +1,23 @@
-An automated smart contract for ride hailing apps like uber using the zksync sepolia testnet
+# UBER Z
 
+## Overview
+
+An automated smart contract for ride hailing apps like uber using the arbitrum sepolia testnet.
+
+## Steps
+
+- owner address in constructor ✅
+- customer / driver database??  driver/customer sign up?
 - given starting location and ending location
 - given the distance 
 - given the waiting time by either driver or passenger
 - price converters from chainlink oracle
 - admin permissions
 - automated payments
+
+
+## Challenges
+
+- representing latitude and longitude in solidity
+- how to maintain a driver/passenger database with finding ability
+- 
--- src/uber.sol
+++ src/uber.sol
@@ -16,9 +16,56 @@ pragma solidity ^0.8.24;
 contract Uber {
     // Type Declarations
     // State Variables
+    address private immutable i_owner; // Address of the contract owner
+
+    uint256 private constant RESOLUTION = 1000000; // Coordinates are calculated by dividing with RESOLUTION
+
+    // Restricted options for the gender of the Passenger/Driver
+    enum Gender {
+        MALE,
+        FEMALE
+    }
+
+    struct Point {
+        int256 lat; // Latitude of the point with 6 decimal points resolution
+        int256 long; // Longitude of the point with 6 decimal point resolution
+    }
+
+    struct Trip {
+        address driverAddress; // Address of the driver
+        address passengerAddress; // Address of the passenger
+        Point startingPoint; // Coordinates of the starting point
+        Point endingPoint; // Coordinates of the ending point
+        uint256 distance; // Distance of the ride
+        uint256 fare; // Total fare for the ride
+    }
+
+    struct DriverInfo {
+        string name; // Name of the driver
+        Gender gender; // Gender of the driver
+        string vehicleLicensePlate; // Vehicle's license plate number
+        bool isActive; // Indicates if the driver is currently active
+    }
+
+    struct PassengerInfo {
+        string name; // Name of the passenger
+        Gender gender; // Gender of the passenger
+    }
+
+    // A mapping of addresses to the information of Drivers
+    mapping(address => DriverInfo) private driverDatabase;
+
+    // A mapping of addresses to the information of Passengers
+    mapping(address => PassengerInfo) private passengerDatabase;
+
     // Modifiers
+
     // Functions Order:
     //// constructor
+
+    constructor() {
+        i_owner = msg.sender;
+    }
     //// receive
     //// fallback
     //// external
