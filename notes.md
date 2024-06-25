1f442955466e9448980555853196402d34943f0a
alfarseniko <alfarseniko@gmail.com>
2024-06-25 22:20:37 +0330

    update: payment function initiated

--- notes.md
+++ notes.md
@@ -8,7 +8,7 @@ An automated smart contract for ride hailing apps like uber using the arbitrum s
 
 - owner address in constructor ✅
 - customer / driver database??  driver/customer sign up? ✅
-- formula which takes distance and gives fare 
+- formula which takes distance and gives fare ✅
 - price converters from chainlink oracle ✅
 - admin permissions ✅
 - automated payments
@@ -19,6 +19,9 @@ An automated smart contract for ride hailing apps like uber using the arbitrum s
 - representing latitude and longitude in solidity
 - how to maintain a driver/passenger database with finding ability
 - convert ARB/USD oracle price to USD/ARB price
+- how to keep track of addresses and permissions of different drivers and their passengers for each trip
+- payment of each combination of drivers and passengers
+- how do i create a modifier which prevents everyone from starting a trip 
 
 
 ARB/USD Oracle Address on Arb Sepolia
--- src/Uber.sol
+++ src/Uber.sol
@@ -10,9 +10,9 @@ import {PriceConverter} from "./PriceConverter.sol";
 // Interfaces, Libraries and Contracts
 
 /**
- @title An automated uber payment app
- @author Haris Waheed Bhatti
- @notice This contract is for the payment of fares after rides
+  * @title An automated uber payment app
+  * @author Haris Waheed Bhatti
+  * @notice This contract is for the payment of fares after rides
          The contract is being made for US Dollars and payments made in Arbitrum
  */
 
@@ -38,22 +38,30 @@ contract Uber {
     struct Trip {
         address driverAddress; // Address of the driver
         address passengerAddress; // Address of the passenger
-        Point startingPoint; // Coordinates of the starting point
-        Point endingPoint; // Coordinates of the ending point
+        uint256 startTime;
+        uint256 stopTime;
         uint256 distance; // Distance of the ride
         uint256 fare; // Total fare for the ride
     }
 
+    Trip[] private tripArray;
+
     struct DriverInfo {
         string name; // Name of the driver
         string vehicleLicensePlate; // Vehicle's license plate number
+        address payable walletAddress; // The address in which payments will be sent
+    }
+
+    struct PassengerInfo {
+        string name; // Name of the passenger
+        address payable walletAddress; // The address in which payments will be sent
     }
 
     // A mapping of addresses to the information of Drivers
     mapping(address => DriverInfo) private s_driverDatabase;
 
     // A mapping of addresses to the information of Passengers
-    mapping(address => string) private s_passengerDatabase;
+    mapping(address => PassengerInfo) private s_passengerDatabase;
 
     // An instance of the contract AggregatorV3Interface
     AggregatorV3Interface private s_priceFeed;
@@ -67,7 +75,7 @@ contract Uber {
     // Functions Order:
     //// constructor
     /**
-    @dev Always input USD price with 36 decimals
+     * @dev Always input USD price with 36 decimals
      */
     constructor(
         uint256 _initialRatePerKm,
@@ -82,45 +90,107 @@ contract Uber {
 
     //// external
 
-    // This function registers a driver with info in the smart contract
+    /**
+     * @notice This function registers a driver with info in the smart contract
+     * @param _name of driver and the license plate number of the car
+     */
     function registerDriver(
         string memory _name,
-        string memory _license
+        string memory _license,
+        address payable _wallet
     ) external {
-        s_driverDatabase[msg.sender] = DriverInfo(_name, _license);
+        require(
+            s_driverDatabase[msg.sender].walletAddress == address(0),
+            "Driver already registered!"
+        );
+        s_driverDatabase[msg.sender] = DriverInfo(_name, _license, _wallet);
     }
 
-    // This function registers a passenger with info in the smart contract
-    function registerPassenger(string memory _name) external {
-        s_passengerDatabase[msg.sender] = _name;
+    /**
+     * @notice This function registers a passenger with info in the smart contract
+     * @param _name of the passenger
+     */
+    function registerPassenger(
+        string memory _name,
+        address payable _wallet
+    ) external {
+        require(
+            s_passengerDatabase[msg.sender].walletAddress == address(0),
+            "Passenger already registered!"
+        );
+        s_passengerDatabase[msg.sender] = PassengerInfo(_name, _wallet);
     }
 
-    // This function allows the owner to change the rate per KM for the fare
+    /**
+     * @notice This function allows the owner to change the rate per KM for the fare
+     * @param _newRate for the rate per km
+     * @dev rate should be in usd expressed as 1e36 or 36 decimals
+     */
     function changeRatePerKm(uint256 _newRate) external onlyOwner {
         s_ratePerKm = _newRate;
     }
 
-    // This function allows the owner to change the baseFare
+    /**
+     * @notice This function allows the owner to change the baseFare
+     * @param _baseFare price for the base fare
+     * @dev rate should be in usd expressed as 1e36 or 36 decimals
+     */
     function changeBaseFare(uint256 _baseFare) external onlyOwner {
         s_baseFare = _baseFare;
     }
     //// public
 
     /**
-    @dev Distance should always be in whole numbers represented in metres (m)
+     * @notice This function allows the owner to change the baseFare
+     * @param _distanceInMetre is the distance of the whole trip in metres
+     * @dev Distance should always be in whole numbers represented in 1000 * Kilometres
+     * @return the total fare price after calculation of fare
      */
-    function calculateFare(uint256 _distanceInMetre) public pure returns (uint256) {
-        uint256 _basefare = convertUsdToArb(s_baseFare);;
+    function calculateFare(
+        uint256 _distanceInMetre
+    ) public view returns (uint256) {
+        uint256 _basefare = convertUsdToArb(s_baseFare);
         uint256 _ratePerKm = convertUsdToArb(s_ratePerKm);
         // Rates and base fare has been converted to ARB represented as 1e18
 
         uint256 _farePrice = _basefare + (_ratePerKm * _distanceInMetre);
+        return _farePrice;
     }
-    //// internal
-    function convertUsdToArb(uint256 _usdAmount) internal view returns (uint256) {
+
+    /**
+     * @notice This function allows functions to easily convert USD to ARB
+     * @param _usdAmount to be converted to ARB
+     * @dev USD amount should be in usd expressed as 1e36 or 36 decimals
+     * @return Price in ARB converted from USD
+     */
+    function convertUsdToArb(
+        uint256 _usdAmount
+    ) internal view returns (uint256) {
         uint256 convertedAmount = _usdAmount.getConversionRate(s_priceFeed);
         return convertedAmount;
     }
+
+    function startTrip(
+        address _driverAddress,
+        address _passengerAddress
+    ) public onlyOwner {
+        Trip memory newTrip;
+
+        newTrip.driverAddress = _driverAddress;
+        newTrip.passengerAddress = _passengerAddress;
+        newTrip.startTime = block.timestamp;
+
+        tripArray.push(newTrip);
+    }
+
     //// private
     //// view / pure
+    /**
+    get driver info
+    get passenger info
+    get base fare
+    get rate per km
+    get conversion rate
+    get owner
+     */
 }
